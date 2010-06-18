6df9a98b1d936b412ec7e9bd3e4f0c675a868b90
Jimmy Shedlick <shedlickj@gmail.com>
2010-06-18 11:18:02 -0400

    remove will_paginate dep from config/enviro

--- config/environment.rb
+++ config/environment.rb
@@ -19,7 +19,7 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem 'mislav-will_paginate', :lib=>'will_paginate', :source=>'http://gems.github.com'
+  # config.gem 'mislav-will_paginate', :lib=>'will_paginate', :source=>'http://gems.github.com'
   #require "will_paginate"
   #WillPaginate::ViewHelpers.pagination_options[:renderer] = 'RemoteLinkRenderer'
 
